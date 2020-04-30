3c2806a192f65f56381e41f2be42db7f5d959a7e
Ega <egaliyando@MacBook-Pro-Ega.local>
2020-04-30 20:43:49 +0700

    Output COnditionals

--- src/Component/List.jsx
+++ src/Component/List.jsx
@@ -4,19 +4,27 @@ import React from "react";
 
 const List = (props) => {
   const { codeathome } = props;
-  const list = codeathome.map((person) => {
-    return (
-      <div
-        key={person.key}
-        style={{ boxShadow: "0px 0px 3px 2px gray", margin: 20, padding: 20, width: 300 }}
-      >
-        <p>Nama {person.nama}</p>
-        <p>Umur {person.umur}</p>
-        <p>Passion {person.passion}</p> <br />
-      </div>
-    );
-  });
-  return <div>{list}</div>;
+  return (
+    <div>
+      {codeathome.map((person) => {
+        return person.umur < 20 ? (
+          <div
+            key={person.key}
+            style={{
+              boxShadow: "0px 0px 3px 2px gray",
+              margin: 20,
+              padding: 20,
+              width: 300,
+            }}
+          >
+            <p>Nama {person.nama}</p>
+            <p>Umur {person.umur}</p>
+            <p>Passion {person.passion}</p> <br />
+          </div>
+        ) : null;
+      })}
+    </div>
+  );
 };
 
 export default List;
--- src/Container/OutputList.jsx
+++ src/Container/OutputList.jsx
@@ -5,15 +5,14 @@ import List from "../Component/List";
 export default class OutputList extends React.Component {
   state = {
     codeathome: [
-      { nama: "Rahmat", umur: 20, passion: "security", key: 1 },
-      { nama: "Ega", umur: 20, passion: "Designer", key: 2 },
-      { nama: "Elfin", umur: 20, passion: "Programmer", key: 3 },
+      { nama: "Rahmat", umur: 21, passion: "security", key: 1 },
+      { nama: "Ega", umur: 19, passion: "Designer", key: 2 },
+      { nama: "Elfin", umur: 18, passion: "Programmer", key: 3 },
     ],
   };
   render() {
     return (
       <div>
-        {" "}
         <h1>Output List</h1> <List codeathome={this.state.codeathome} />
       </div>
     );
